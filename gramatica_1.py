49794230e5b1920cfb28a17336ea5ec232495073
Camilo Millan <camimillan2003@gmail.com>
2024-02-13 21:13:18 -0500

    Adición de los 3 archivos para el primer verificador gramatical

--- gramatica_1.py
+++ gramatica_1.py
@@ -10,8 +10,30 @@ def es_binario_capicua(cadena):
             return False
     return True
 
-cadena = input("Ingrese una cadena binaria: ")
-if es_binario_capicua(cadena):
-    print("La cadena es aceptada.")
-else:
-    print("La cadena NO es aceptada.")
+def leer_binario_desde_archivo(ruta_archivo):
+    try:
+        with open(ruta_archivo, 'r') as archivo:
+            contenido = archivo.read()
+            return contenido.strip()
+    except FileNotFoundError:
+        print(f"No se encontró el archivo: {ruta_archivo}")
+        return None
+
+# Definir las rutas de los tres archivos
+rutas_archivos = [
+    "programa_de_verificación_gramatical\Gramatica1_Correcta.txt",
+    "programa_de_verificación_gramatical\Gramatica1_Incorrecta.txt",
+    "programa_de_verificación_gramatical\Gramatica1_IncorrectaCaracter.txt"
+]
+
+# Procesar cada archivo y aplicar la función es_binario_capicua a su contenido
+for ruta_archivo in rutas_archivos:
+    print(f"\nVerificando el archivo: {ruta_archivo}")
+    cadena = leer_binario_desde_archivo(ruta_archivo)
+
+    if cadena is not None:
+        if es_binario_capicua(cadena):
+            print("La cadena es aceptada.")
+        else:
+            print("La cadena NO es aceptada.")
+
